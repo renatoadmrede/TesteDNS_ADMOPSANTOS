c4db5a4050c9f4da117e2736f9dc672159e45f08
Renato Santos <renatoestudosemprel2025@gmail.com>
2025-09-18 11:23:24 -0300

    1 Teste DNS

---
+++ variables.tf
@@ -0,0 +1,8 @@
+variable "920279090818" {
+  description = "ID do projeto no GCP"
+  type        = string
+}
+variable "dns_zone" {
+  description = "Nome da zona DNS já criada no GCP"
+  type        = string
+}
