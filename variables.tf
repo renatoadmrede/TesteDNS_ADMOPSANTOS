804a6ff5e4c20f78360a35523da770e42e60f371
Renato Santos <renatoestudosemprel2025@gmail.com>
2025-09-19 08:53:38 -0300

    Signed-off-by: Renato Santos <renatoestudosemprel2025@gmail.com>

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable "project_id" {
-  description = "ID do projeto no GCP"
-  type        = string
-}
-variable "dns_zone" {
-  description = "Nome da zona DNS já criada no GCP"
-  type        = string
-}
